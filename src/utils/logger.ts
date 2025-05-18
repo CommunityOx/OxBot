c91f0244d0ad3f48b40ae8fc981ed1e8c35ce1ce
ANTOND. <71350868+antond15@users.noreply.github.com>
2025-05-19 01:33:46 +0200

    feat(utils/logger): concate consecutive parameters

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,15 @@
-import pino from 'pino';
+import pino, { LogFn, Logger } from 'pino';
+
+function logMethod(this: Logger, args: Parameters<LogFn>, method: LogFn) {
+  if (args.length === 2) {
+    args[0] = `${args[0]} %j`;
+  }
+  method.apply(this, args);
+}
 
 const logger = pino({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  hooks: { logMethod },
   transport: {
     target: 'pino-pretty',
     options: {
