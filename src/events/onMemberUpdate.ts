ff7923fcbdd58897c25942119229ff9e52f66170
ANTOND. <71350868+antond15@users.noreply.github.com>
2025-06-17 18:27:26 +0200

    feat(events/onMemberUpdate): check for cache mismatch after 2500ms delay

--- src/events/onMemberUpdate.ts
+++ src/events/onMemberUpdate.ts
@@ -1,8 +1,15 @@
 import { GuildMember, PartialGuildMember } from 'discord.js';
+import { setTimeout as wait } from 'node:timers/promises';
 import { Roles } from '../constants';
 import logger from '../utils/logger';
 
 export const onMemberUpdate = async (oldMember: GuildMember | PartialGuildMember, newMember: GuildMember) => {
+  const before_newRoles = newMember.roles.cache;
+  const before_hasGitHubRole = before_newRoles.has(Roles.GitHub);
+  const before_hasWardenTagRole = before_newRoles.has(Roles.WardenTag);
+
+  await wait(2500);
+
   const newRoles = newMember.roles.cache;
 
   const hasGitHubRole = newRoles.has(Roles.GitHub);
@@ -10,6 +17,11 @@ export const onMemberUpdate = async (oldMember: GuildMember | PartialGuildMember
   const hasSupportAccess = newRoles.has(Roles.SupportAccess);
 
   const shouldHaveSupportAccess = hasGitHubRole && !hasWardenTagRole;
+
+  if (hasGitHubRole !== before_hasGitHubRole || hasWardenTagRole !== before_hasWardenTagRole) {
+    const oldRoles = oldMember.roles.cache;
+    logger.info(`Cache mismatch for ${newMember.user.id} - ${oldRoles.has(Roles.GitHub)}:${before_hasGitHubRole}:${hasGitHubRole}, ${oldRoles.has(Roles.SupportAccess)}:${before_hasWardenTagRole}:${hasWardenTagRole}`);
+  }
 
   if (hasSupportAccess && !shouldHaveSupportAccess) {
     // remove Support Access role (user unlinked their GitHub or got tagged by Warden)
@@ -27,7 +39,5 @@ export const onMemberUpdate = async (oldMember: GuildMember | PartialGuildMember
       const errorMessage = error instanceof Error ? error.message : String(error);
       logger.error(`Failed to add role to ${newMember.user.username}: ${errorMessage}`);
     }
-  } else {
-    logger.info(`No role change for ${newMember.user.username}. GitHub: ${hasGitHubRole}; Warden Tag: ${hasWardenTagRole}`);
   }
 };
