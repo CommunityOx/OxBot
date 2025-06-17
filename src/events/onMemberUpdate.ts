f2b3ec6f0db9a4e58297d37075cfae9b53efdb95
ANTOND. <71350868+antond15@users.noreply.github.com>
2025-06-17 18:26:11 +0200

    feat(events/onMemberUpdate): improve code structure

--- src/events/onMemberUpdate.ts
+++ src/events/onMemberUpdate.ts
@@ -3,30 +3,31 @@ import { Roles } from '../constants';
 import logger from '../utils/logger';
 
 export const onMemberUpdate = async (oldMember: GuildMember | PartialGuildMember, newMember: GuildMember) => {
-  const hasGitHubRole = newMember.roles.cache.has(Roles.GitHub);
-  const hasWardenTagRole = newMember.roles.cache.has(Roles.WardenTag);
+  const newRoles = newMember.roles.cache;
 
-  if (newMember.roles.cache.has(Roles.SupportAccess)) {
+  const hasGitHubRole = newRoles.has(Roles.GitHub);
+  const hasWardenTagRole = newRoles.has(Roles.WardenTag);
+  const hasSupportAccess = newRoles.has(Roles.SupportAccess);
+
+  const shouldHaveSupportAccess = hasGitHubRole && !hasWardenTagRole;
+
+  if (hasSupportAccess && !shouldHaveSupportAccess) {
     // remove Support Access role (user unlinked their GitHub or got tagged by Warden)
-    if (!hasGitHubRole || hasWardenTagRole) {
-      try {
-        await newMember.roles.remove(Roles.SupportAccess);
-      } catch (error: unknown) {
-        const errorMessage = error instanceof Error ? error.message : String(error);
-        logger.error(`Failed to remove role role ${newMember.user.username}: ${errorMessage}`);
-      }
+    try {
+      await newMember.roles.remove(Roles.SupportAccess);
+    } catch (error: unknown) {
+      const errorMessage = error instanceof Error ? error.message : String(error);
+      logger.error(`Failed to remove role role ${newMember.user.username}: ${errorMessage}`);
+    }
+  } else if (!hasSupportAccess && shouldHaveSupportAccess) {
+    // add Support Access role (user linked their GitHub or got untagged by Warden)
+    try {
+      await newMember.roles.add(Roles.SupportAccess);
+    } catch (error: unknown) {
+      const errorMessage = error instanceof Error ? error.message : String(error);
+      logger.error(`Failed to add role to ${newMember.user.username}: ${errorMessage}`);
     }
   } else {
-    // add Support Access role (user linked their GitHub or got untagged by Warden)
-    if (hasGitHubRole && !hasWardenTagRole) {
-      try {
-        await newMember.roles.add(Roles.SupportAccess);
-      } catch (error: unknown) {
-        const errorMessage = error instanceof Error ? error.message : String(error);
-        logger.error(`Failed to add role to ${newMember.user.username}: ${errorMessage}`);
-      }
-    } else {
-      logger.info(`No role change for ${newMember.user.username}. GitHub: ${hasGitHubRole}; Warden Tag: ${hasWardenTagRole}`);
-    }
+    logger.info(`No role change for ${newMember.user.username}. GitHub: ${hasGitHubRole}; Warden Tag: ${hasWardenTagRole}`);
   }
 };
